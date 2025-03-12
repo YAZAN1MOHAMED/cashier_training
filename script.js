d151f4f5613a35f1ecbe022d65c57c22c488e893
YAZAN1MOHAMED <yazan4m2@gmail.com>
2025-03-13 03:37:57 +0400

    Add the logic to correct and wrong answer function , improve appearing style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const billValue = document.querySelector('.bill-value')
 const amountPaid = document.querySelector('.amount-paid')
 const answerInput = document.querySelector('.answer-input')
 const submitButton = document.querySelector('.submit-button')
-const answerForm =  document.querySelector('#answer-form')
+const answerForm = document.querySelector('#answer-form')
 
 
 //Start Random Generation Section
@@ -25,21 +25,30 @@ changeQuestion(billValue, amountPaid);
 // End Random Generation Section.
 
 // Start calculation Section
-answerForm.addEventListener('submit',(e)=>{
+answerForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    checkAnswer(billValue.innerHTML,amountPaid.innerHTML,answerInput.value)})
+    checkAnswer(billValue.innerHTML, amountPaid.innerHTML, answerInput.value)
+})
 
-const wrongAnswer=()=>{
-    const message = document.querySelector('.wrongAnswer')
+const wrongAnswer = (correctValue) => {
+
+    const wrongAnswer = document.querySelector('.wrong-answer');
+    wrongAnswer.innerHTML = `The answer is wrong ❌ the correct answer is : ${correctValue}`
+    const correctAnswer = document.querySelector('.correct-answer');
+    wrongAnswer.classList.remove('hide')
+    correctAnswer.classList.add('hide');
 }
-const correctAnswer=()=>{
-    const correctAnswer = document.querySelector('.correctAnswer')
+const correctAnswer = () => {
+    const wrongAnswer = document.querySelector('.wrong-answer');
+    const correctAnswer = document.querySelector('.correct-answer')
+    wrongAnswer.classList.add('hide')
     correctAnswer.classList.remove('hide');
 }
-const checkAnswer = (billValue,amountPaid,inputValue)=>{
-const isAnswerTrue = amountPaid - billValue == inputValue;
-
-if (isAnswerTrue) {
-    correctAnswer()
-}
+const checkAnswer = (billValue, amountPaid, inputValue) => {
+    const change = amountPaid - billValue;
+    const isAnswerTrue = change == inputValue;
+    if (isAnswerTrue) {
+        correctAnswer()
+    }
+    else wrongAnswer(change)
 }
